02b9ad860e2bbfc890b17401f39b05005ba99524
A6897 <anarghsidhardhan97@gmail.com>
2023-11-13 01:58:02 -0500

    added the scrollbar and modified textbox to figma format

--- kaleidoscope_collaborative/lib/screens/signup1_1.dart
+++ kaleidoscope_collaborative/lib/screens/signup1_1.dart
@@ -58,140 +58,151 @@ class _SignupScreenState extends State<SignupScreen> {
         backgroundColor: Colors.white,
         elevation: 0,
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Image.asset(
-                'images/logo.jpg',
-                width: 117.0, // Set the width to match your design
-                height: 99.0, // Set the height to match your design
-              ),
-              SizedBox(height: _fnameFocus.hasFocus || _lnameFocus.hasFocus || _usernameFocus.hasFocus ? 20 : 48),
-
-              // First Name input
-              TextField(
-                focusNode: _fnameFocus,
-                decoration: InputDecoration(
-                  labelText: 'First Name',
-                  suffixIcon: Icon(Icons.close),
+      body: Scrollbar(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Image.asset(
+                  'images/logo.jpg',
+                  width: 117.0, // Set the width to match your design
+                  height: 99.0, // Set the height to match your design
                 ),
-              ),
-              SizedBox(height: 16),
+                SizedBox(height: _fnameFocus.hasFocus || _lnameFocus.hasFocus || _usernameFocus.hasFocus ? 20 : 48),
 
-              // Last Name input
-              TextField(
-                focusNode: _lnameFocus,
-                obscureText: true,
-                decoration: const InputDecoration(
-                  labelText: 'Last Name',
-                  suffixIcon: Icon(Icons.close),
-                ),
-              ),
-              SizedBox(height: 16),
-
-               // UserName input
-              TextField(
-                focusNode: _usernameFocus,
-                decoration: InputDecoration(
-                  labelText: 'Username',
-                  suffixIcon: Icon(Icons.close),
-                ),
-              ),
-              SizedBox(height: 32),
-
-              // Password input
-              TextField(
-                focusNode: _passwordFocus,
-                obscureText: true,
-                decoration: const InputDecoration(
-                  labelText: 'Password',
-                  suffixIcon: Icon(Icons.close),
-                ),
-              ),
-              SizedBox(height: 16),
-
-              // Confirm Password input
-              TextField(
-                focusNode: _confirmPasswordFocus,
-                obscureText: true,
-                decoration: const InputDecoration(
-                  labelText: ' Confirm Password',
-                  suffixIcon: Icon(Icons.close),
-                ),
-              ),
-              SizedBox(height: 32),
-
-              // Email input
-              TextField(
-                focusNode: _emailFocus,
-                obscureText: true,
-                decoration: const InputDecoration(
-                  labelText: 'Email',
-                  suffixIcon: Icon(Icons.close),
-                ),
-              ),
-              SizedBox(height: 16),
-
-              // Confirm Email input
-              TextField(
-                focusNode: _confirmEmailFocus,
-                obscureText: true,
-                decoration: const InputDecoration(
-                  labelText: ' Confirm Email',
-                  suffixIcon: Icon(Icons.close),
-                ),
-              ),
-              SizedBox(height: 32),
-
-              
-
-              // Divider with 'or'
-              const Row(
-                children: <Widget>[
-                  Expanded(child: Divider(
-                    color: Colors.black,
-                    thickness: 1,
-                  )),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 8),
-                    child: Text('or'),
+                // First Name input
+                TextField(
+                  focusNode: _fnameFocus,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'First Name',
+                    suffixIcon: Icon(Icons.close),
                   ),
-                  Expanded(child: Divider(
-                    color: Colors.black,
-                    thickness: 1,
-                  )),
+                ),
+                SizedBox(height: 16),
+
+                // Last Name input
+                TextField(
+                  focusNode: _lnameFocus,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Last Name',
+                    suffixIcon: Icon(Icons.close),
+                  ),
+                ),
+                SizedBox(height: 16),
+
+                // UserName input
+                TextField(
+                  focusNode: _usernameFocus,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Username',
+                    suffixIcon: Icon(Icons.close),
+                  ),
+                ),
+                SizedBox(height: 32),
+
+                // Password input
+                TextField(
+                  focusNode: _passwordFocus,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    suffixIcon: Icon(Icons.close),
+                  ),
+                ),
+                SizedBox(height: 16),
+
+                // Confirm Password input
+                TextField(
+                  focusNode: _confirmPasswordFocus,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: ' Confirm Password',
+                    suffixIcon: Icon(Icons.close),
+                  ),
+                ),
+                SizedBox(height: 32),
+
+                // Email input
+                TextField(
+                  focusNode: _emailFocus,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                    suffixIcon: Icon(Icons.close),
+                  ),
+                ),
+                SizedBox(height: 16),
+
+                // Confirm Email input
+                TextField(
+                  focusNode: _confirmEmailFocus,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: ' Confirm Email',
+                    suffixIcon: Icon(Icons.close),
+                  ),
+                ),
+                SizedBox(height: 32),
+
+                
+
+                // Divider with 'or'
+                const Row(
+                  children: <Widget>[
+                    Expanded(child: Divider(
+                      color: Colors.black,
+                      thickness: 1,
+                    )),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 8),
+                      child: Text('or'),
+                    ),
+                    Expanded(child: Divider(
+                      color: Colors.black,
+                      thickness: 1,
+                    )),
+                  ],
+                ),
+                SizedBox(height: 16),
+
+                // Phone Number input
+                TextField(
+                  focusNode: _phoneNumberFocus,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Phone Number',
+                    suffixIcon: Icon(Icons.close),
+                  ),
+                ),
+                SizedBox(height: 16),
+
+                // Confirm Phone Number input
+                TextField(
+                  focusNode: _confirmPhoneNumberFocus,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: ' Confirm Phone Number',
+                    suffixIcon: Icon(Icons.close),
+                  ),
+                ),
+                SizedBox(height: 32),
+
+
                 ],
               ),
-              SizedBox(height: 16),
-
-              // Phone Number input
-              TextField(
-                focusNode: _phoneNumberFocus,
-                obscureText: true,
-                decoration: const InputDecoration(
-                  labelText: 'Phone Number',
-                  suffixIcon: Icon(Icons.close),
-                ),
-              ),
-              SizedBox(height: 16),
-
-              // Confirm Phone Number input
-              TextField(
-                focusNode: _confirmPhoneNumberFocus,
-                obscureText: true,
-                decoration: const InputDecoration(
-                  labelText: ' Confirm Phone Number',
-                  suffixIcon: Icon(Icons.close),
-                ),
-              ),
-              SizedBox(height: 32),
-
-
-            ],
           ),
         ),
       ),
