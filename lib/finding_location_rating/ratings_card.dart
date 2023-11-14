4c5b7599bdb057f9a85e053fc72887e2c07bacbe
JingHong Huang <jinghong@bu.edu>
2023-11-13 22:39:55 -0500

    formatting the rating page

--- lib/finding_location_rating/ratings_card.dart
+++ lib/finding_location_rating/ratings_card.dart
@@ -15,37 +15,56 @@ class _RatingPageState extends State<RatingPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Rating Cards'),
+        title: const Text('Rating Cards'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(height: 30),
+            const SizedBox(height: 20),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween, 
               children: [
-                Image.asset('images/logo.jpg', height: 100, width: 100),
-                Column(
+                Image.asset('images/logo.jpg', height: 125, width: 125),
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     RatingBar(),
-                    SizedBox(height: 10),
+                    SizedBox(height: 5),
                     // Left align the text 
-                    Text('John Doe', style: TextStyle(fontSize: 20), textAlign: TextAlign.left),
-
+                    Padding(
+                      padding: EdgeInsets.only(left: 15),
+                      child: Text('Joe Smith', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                    ),
                   ],
                 ),
               ],
             ),
-            SizedBox(height: 30),
-            ElevatedButton(
-              onPressed: () {
-                inAppReview.requestReview();
-              },
-              child: Text('Rate Now'),
-            ),
+            const SizedBox(height: 20),
+            // Pad the text to the right 
+            const Padding(
+              padding: EdgeInsets.only(left: 40, right: 40),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'I’ve been going for a few weeks and my son loves it. He gets to try new things in a safe environment. It also allows him to spend some time wit...',
+                    style: TextStyle(fontSize: 22),
+                  ),
+                  SizedBox(height: 8), // Add spacing between the two text lines
+                  Text(
+                    'Read more',
+                    style: TextStyle(
+                      fontSize: 18,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ],
+              ),
+            ),        
           ],
+      ),
       ),
     );
   }
@@ -54,32 +73,82 @@ class _RatingPageState extends State<RatingPage> {
 class RatingBar extends StatefulWidget
  
 {
+  const RatingBar({super.key});
+
   @override
   _RatingBarState createState() => _RatingBarState();
 }
 
+// Rating bar: 
 class _RatingBarState extends State<RatingBar> {
   int rating = 0;
+
   @override
   Widget build(BuildContext context) {
     return Row(
-      mainAxisAlignment: MainAxisAlignment.end, 
       children: [
         for (int i = 1; i <= 5; i++)
-          IconButton(
-            icon: Icon(Icons.star, size: 30, color: rating >= i ? Colors.yellow : Colors.grey),
-            onPressed: () {
-              setState(() {
-                rating = i;
-              });
-            },
+          Container(
+            width: 45, // Adjust the width to control the size of the circle
+            height: 45, // Adjust the height to control the size of the circle
+            margin: const EdgeInsets.symmetric(horizontal: 6), // Adjust the spacing between stars
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              color: Color.fromRGBO(47, 10, 158, 0.612) // Background color of the circle
+            ),
+            child: IconButton(
+              icon: Icon(Icons.star, size: 30, color: rating >= i ? Colors.yellow : Colors.white),
+              onPressed: () {
+                setState(() {
+                  rating = i;
+                });
+              },
+            )
           ),
-          // add white space to push the rating bar to the right 
-          SizedBox(width: 40), 
-
       ],
-      // Pad the end with spaces to push the rating bar to the right 
-    
+    );
+  }
+}
+
+
+// Feature boxes 
+
+class FeatureBoxes extends StatefulWidget
+ 
+{
+  const FeatureBoxes({super.key});
+
+  @override
+  _FeatureBoxesState createState() => _FeatureBoxesState();
+}
+
+// Rating bar: 
+class _FeatureBoxesState extends State<FeatureBoxes> {
+  int rating = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        for (int i = 1; i <= 5; i++)
+          Container(
+            width: 45, // Adjust the width to control the size of the circle
+            height: 45, // Adjust the height to control the size of the circle
+            margin: const EdgeInsets.symmetric(horizontal: 6), // Adjust the spacing between stars
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              color: Color.fromRGBO(47, 10, 158, 0.612) // Background color of the circle
+            ),
+            child: IconButton(
+              icon: Icon(Icons.star, size: 30, color: rating >= i ? Colors.yellow : Colors.white),
+              onPressed: () {
+                setState(() {
+                  rating = i;
+                });
+              },
+            )
+          ),
+      ],
     );
   }
 }
