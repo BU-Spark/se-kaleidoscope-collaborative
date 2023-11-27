395015ced5a43d43cbc9f63ebec597fba9bf371c
JingHong Huang <jinghong@bu.edu>
2023-11-27 01:17:10 -0500

    Debugging search page logic

---
+++ README.md
@@ -0,0 +1,28 @@
+## Keep flutter version 3.13.9 
+
+## Command + k: toggle the keyboard in the emulator 
+
+## Installing Flutter on Mac
+
+1. Download the Flutter SDK from the following link: https://flutter.dev/docs/get-started/install/macos
+
+2. Extract the file in the desired location 
+
+3. Add the flutter tool to your path: 
+``` 
+export PATH="$PATH:`pwd`/flutter/bin"
+``` 
+4. Run the following command to see if there are any dependencies you need to install to complete the setup: 
+``` 
+
+flutter doctor
+``` 
+5. Run the following command to update your path: 
+``` 
+source ~/.bash_profile
+```
+
+## Installing Flutter on Windows: see https://flutter.dev/docs/get-started/install/windows 
+
+
+## Installing Flutter on Linux: see https://flutter.dev/docs/get-started/install/linux 
--- lib/finding_location_rating/search_page_1_0.dart
+++ lib/finding_location_rating/search_page_1_0.dart
@@ -7,40 +7,123 @@ class SearchPage extends StatefulWidget {
 
 class _SearchPageState extends State<SearchPage> {
   final TextEditingController _searchController = TextEditingController();
-  final FocusNode _searchFocus = FocusNode(); // Create a FocusNode
-  List<String> _searchHistory = []; // Define _searchHistory here
+  final FocusNode _searchFocus = FocusNode();
+  List<String> _searchHistory = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: TextField(
-          controller: _searchController,
-          focusNode: _searchFocus, // Assign the FocusNode to the TextField
-          decoration: InputDecoration(
-            hintText: 'Search',
-            border: InputBorder.none,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(kToolbarHeight),
+        child: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+            },
           ),
-          onTap: () {
-            FocusScope.of(context).requestFocus(_searchFocus); // Request focus when tapping the TextField
-          },
-          onSubmitted: (query) {
-            setState(() {
-              _searchHistory.add(query);
-              _searchController.clear();
-            });
-            // TODO: Perform search and display results
-          },
+          actions: [
+            GestureDetector(
+              child: Icon(Icons.history),
+              onTap: () {},
+            ),
+          ],
+          backgroundColor: Colors.blue,
+          elevation: 0,
         ),
-        actions: [
-          GestureDetector(
-            child: Icon(Icons.history),
-            onTap: () {},
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: kToolbarHeight + 8),
+            child: Container(
+              height: 40,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              margin: EdgeInsets.symmetric(horizontal: 16),
+              child: TextField(
+                controller: _searchController,
+                focusNode: _searchFocus,
+                decoration: InputDecoration(
+                  hintText: 'Search',
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.symmetric(horizontal: 16),
+                ),
+                onTap: () {
+                  FocusScope.of(context).requestFocus(_searchFocus);
+                },
+                onSubmitted: (query) {
+                  setState(() {
+                    _searchHistory.add(query);
+                    _searchController.clear();
+                  });
+                  // TODO: Perform search and display results
+                  // route this to firebase database and display results
+                },
+              ),
+            ),
+          ),
+          SizedBox(height: 16),
+          _buildRecentSearches(),
+          Padding(
+            padding: EdgeInsets.only(top: 16),
+            child: Center(
+              child: Text('Search results go here'),
+            ),
           ),
         ],
       ),
-      body: Center(
-        child: Text('Search results go here'),
-      ),
     );
+  }
+
+  Widget _buildRecentSearches() {
+    return _searchHistory.isNotEmpty
+        ? Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Text(
+                  'Recent Searches',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              SizedBox(height: 8),
+              Container(
+                height: _searchHistory.length * 50.0, // Adjust the height based on the number of items
+                child: ListView.builder(
+                  itemCount: _searchHistory.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Chip(
+                            label: Text(_searchHistory[index]),
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.clear),
+                            onPressed: () {
+                              setState(() {
+                                _searchHistory.removeAt(index);
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          )
+        : Container();
   }
 }
