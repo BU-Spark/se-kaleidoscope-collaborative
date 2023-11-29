6234b319df92fa8faee24ca0c63922205394696f
A6897 <anarghsidhardhan97@gmail.com>
2023-11-29 10:56:04 -0500

    added the parameter rating bar

--- kaleidoscope_collaborative/lib/screens/AddRating/review_page_3_1_paramterRatingPage.dart
+++ kaleidoscope_collaborative/lib/screens/AddRating/review_page_3_1_paramterRatingPage.dart
@@ -30,7 +30,46 @@ class _ParameterRatingPageState extends State<ParameterRatingPage> {
               'How would you rate ${widget.parameterName} at this business?',
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
+            // Star rating widget, similar to the OverallRatingPage
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              // mainAxisSize: MainAxisSize.max,
+              children: 
+              List.generate(5, (index) {
+                
+                return IconButton(
+                  icon: Stack(
+                    alignment: Alignment.center,
+                    children: <Widget>[
+                      // Circle icon as the background
+                      Icon(
 
+                        index < parameterRating ? Icons.circle : Icons.circle,
+                        color: index < parameterRating ? Color(0xFF6750A4) : Colors.grey,
+                        size: 60, 
+                      ),
+                      // Star icon on top of the circle
+                      Align(
+                        alignment: Alignment.center,
+                        child: Icon(
+                          Icons.star,
+                          color: Colors.white, // The color for the circle
+                          size: 58, // The size of the circle
+                        ),
+                      ),
+                    ],
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      parameterRating = index + 1;
+                    });
+                  },
+                );
+              }),
+            ),
+            SizedBox(height: 48),
+            // ... (Reuse the star rating widget code here)
             // Next button and back to business page
             SizedBox(height: 20),
             Row(
