9798342ed96dcc0c692283e055d12a59bebef704
A6897 <anarghsidhardhan97@gmail.com>
2023-11-29 12:30:10 -0500

    added the organization image and title

--- kaleidoscope_collaborative/lib/screens/AddRating/text_review_4_1.dart
+++ kaleidoscope_collaborative/lib/screens/AddRating/text_review_4_1.dart
@@ -16,7 +16,11 @@ class _TextReviewPageState extends State<TextReviewPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Life Ki Do Martial Arts'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        title: Text('Write a Review Page', style: TextStyle(color: Colors.black)),
         backgroundColor: Colors.white,
         elevation: 0,
       ),
@@ -25,6 +29,42 @@ class _TextReviewPageState extends State<TextReviewPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
+
+            Row(
+              children: <Widget>[
+                // Small Image on the top left corner
+                Image.asset(
+                  'images/dummy.jpg',
+                  width: 117.0, // Set the width to match your design
+                  height: 99.0, // Set the height to match your design
+                ),
+                SizedBox(width: 16.0), // Add some spacing between the image and text
+                // Organization Title and Type
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Organization Name',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Organization Type',
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    SizedBox(height: 48),
+                  ],
+                ),
+              ],
+            ),
+                SizedBox(height: 20),
+            
             Text('Write your review', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
             TextField(
               controller: _controller,
@@ -38,7 +78,8 @@ class _TextReviewPageState extends State<TextReviewPage> {
                 });
               },
             ),
-            Spacer(),
+            SizedBox(height: 20),
+
             ElevatedButton(
               onPressed: _hasWrittenReview ? () {
                 // Implement submit logic with _controller.text
