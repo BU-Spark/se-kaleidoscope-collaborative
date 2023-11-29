ad045ab78e0c464a8cda1037bb486a08091c0c04
A6897 <anarghsidhardhan97@gmail.com>
2023-11-28 20:03:51 -0500

    updated page layout

--- kaleidoscope_collaborative/lib/screens/AddRating/review_page_2_1.dart
+++ kaleidoscope_collaborative/lib/screens/AddRating/review_page_2_1.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:kaleidoscope_collaborative/screens/constants.dart';
 
-// Implementing the 1.1 Review Page
+class ChooseRatingParametersPage extends StatefulWidget {
+  const ChooseRatingParametersPage({Key? key}) : super(key: key);
 
-class ChooseRatingParametersPage extends StatefulWidget{
-  const ChooseRatingParametersPage({super.key});
   @override
   _ChooseRatingParametersPageState createState() => _ChooseRatingParametersPageState();
 }
@@ -14,7 +13,7 @@ class _ChooseRatingParametersPageState extends State<ChooseRatingParametersPage>
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Choose Rating Parameters Page',style: TextStyle(color:Colors.black)),
+        title: Text('Choose Rating Paramters', style: TextStyle(color: Colors.black)),
         leading: IconButton(
           icon: Icon(Icons.arrow_back, color: Colors.black),
           onPressed: () => Navigator.of(context).pop(),
@@ -24,33 +23,88 @@ class _ChooseRatingParametersPageState extends State<ChooseRatingParametersPage>
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
-        child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              // Small Image on the top left corner
-              Image.asset(
-                'images/dummy.jpg',
-                width: 117.0,
-                height: 99.0,
-              ),
-            SizedBox(width: 16.0), // Add some spacing between the image and text
-            // Title and Subtitle on the right, left-aligned
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
               children: <Widget>[
-                Text(
-                  'Title',
-                  style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+                // Small Image on the top left corner
+                Image.asset(
+                  'images/dummy.jpg',
+                  width: 117.0, // Set the width to match your design
+                  height: 99.0, // Set the height to match your design
                 ),
-                SizedBox(height: 8.0),
-                Text(
-                  'Subtitle',
-                  style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w500),
+                SizedBox(width: 16.0), // Add some spacing between the image and text
+                // Organization Title and Type
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Organization Name',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Organization Type',
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    SizedBox(height: 48),
+                  ],
                 ),
               ],
             ),
+            Column( 
+              // crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(height: 20),
+            Text(
+              'What accomodation(s) have you observed here?',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 17,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 48),
+
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ChooseRatingParametersPage(),
+                  ),
+                );
+              },
+              child: Text('Next'),
+              style: kSmallButtonStyle,
+            ),
+            SizedBox(height: 16),
+
+            Align(
+                  alignment: Alignment.center,
+                  child: TextButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    child: Text(
+                      'Back to business page',
+                      style: TextStyle(
+                        decoration: TextDecoration.underline,
+                        color: Color(0xFF6750A4),
+                      ),
+                    ),
+                  ),
+                ),
           ],
         ),
+      ],
+      ),
       ),
     );
   }
