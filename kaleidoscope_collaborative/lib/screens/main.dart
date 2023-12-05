c215a09675e732e56a84ef05da5acc128bfd1041
A6897 <anarghsidhardhan97@gmail.com>
2023-12-04 19:16:07 -0500

    adding the firebase init

--- kaleidoscope_collaborative/lib/screens/main.dart
+++ kaleidoscope_collaborative/lib/screens/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 import 'first_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() => runApp(MyApp());
+// void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
 
 class MyApp extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
