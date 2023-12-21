712a0cd207831dde76d505fa69889bb303ee37c6
Revathi Vipinachandran <revathivch@gmail.com>
2023-12-21 18:38:36 -0500

    Moved the test code for main and first screen to separate test code files

--- kaleidoscope_collaborative/test/home_test.dart
+++ kaleidoscope_collaborative/test/home_test.dart
@@ -5,37 +5,16 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:kaleidoscope_collaborative/screens/firebase_options.dart';
 import 'package:kaleidoscope_collaborative/screens/first_screen.dart';
 
 import 'package:kaleidoscope_collaborative/screens/main.dart';
 
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:kaleidoscope_collaborative/screens/SignupLandingPage.dart';
-import 'package:kaleidoscope_collaborative/screens/LoggingIn/login_screen.dart';
-import 'package:mockito/mockito.dart';
-
-
-import 'package:mockito/mockito.dart';
-// Import the `any` matcher for null-safety
-import 'package:mockito/annotations.dart';
-
-// Use @GenerateMocks to generate the MockNavigatorObserver class
-// @GenerateMocks([NavigatorObserver])
-
-
-// class MockNavigatorObserver extends Mock implements NavigatorObserver {}
 
 void main() {
-  // Mock Firebase initialization before running the tests
-  // setUpAll(() async {
-  //   TestWidgetsFlutterBinding.ensureInitialized();
-  //   await Firebase.initializeApp(
-  //     options: DefaultFirebaseOptions.currentPlatform,
-  //   );
-  // });
 
   testWidgets('MyApp loads FirstScreen as the home', (WidgetTester tester) async {
     // Testing main.dart
@@ -45,7 +24,6 @@ void main() {
     // Verify that FirstScreen is loaded.
     expect(find.byType(FirstScreen), findsOneWidget);
   });
-
 
   // Testing first_screen.dart
   testWidgets('FirstScreen UI Test', (WidgetTester tester) async {
@@ -70,8 +48,5 @@ void main() {
     // Verify the 'Skip for now' text button is displayed
     expect(find.widgetWithText(TextButton, 'Skip for now'), findsOneWidget);
   });
-
-
-  // Testing login_screen UI
 }
 
