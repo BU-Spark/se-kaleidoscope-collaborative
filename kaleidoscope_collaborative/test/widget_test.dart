fec006c0ac961bd120ede738c02ab6c54c939e47
Revathi Vipinachandran <revathivch@gmail.com>
2023-12-21 18:12:04 -0500

    Added test cases to check for first screen UI

--- kaleidoscope_collaborative/test/widget_test.dart
+++ kaleidoscope_collaborative/test/widget_test.dart
@@ -17,9 +17,16 @@ import 'package:kaleidoscope_collaborative/screens/SignupLandingPage.dart';
 import 'package:kaleidoscope_collaborative/screens/LoggingIn/login_screen.dart';
 import 'package:mockito/mockito.dart';
 
-// Create a mock navigator observer
-class MockNavigatorObserver extends Mock implements NavigatorObserver {}
 
+import 'package:mockito/mockito.dart';
+// Import the `any` matcher for null-safety
+import 'package:mockito/annotations.dart';
+
+// Use @GenerateMocks to generate the MockNavigatorObserver class
+// @GenerateMocks([NavigatorObserver])
+
+
+// class MockNavigatorObserver extends Mock implements NavigatorObserver {}
 
 void main() {
   // Mock Firebase initialization before running the tests
@@ -31,57 +38,40 @@ void main() {
   // });
 
   testWidgets('MyApp loads FirstScreen as the home', (WidgetTester tester) async {
+    // Testing main.dart
     // This will test whether your FirstScreen widget is being loaded as the home screen in your MyApp widget
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
     // Verify that FirstScreen is loaded.
     expect(find.byType(FirstScreen), findsOneWidget);
-
-
-    testWidgets('FirstScreen UI Test', (WidgetTester tester) async {
-      // Create the widget by telling the tester to build it
-      await tester.pumpWidget(MaterialApp(home: FirstScreen()));
-
-      // Verify the title is displayed
-      expect(find.text('Kaleidoscope Collaborative'), findsOneWidget);
-
-      // Verify the logo is displayed
-      expect(find.byType(Image), findsOneWidget);
-
-      // Verify the main message is displayed
-      expect(find.text('Discover Disability Inclusive\nServices Around You!'), findsOneWidget);
-
-      // Verify the 'Sign Up' button is displayed
-      expect(find.widgetWithText(ElevatedButton, 'Sign Up'), findsOneWidget);
-
-      // Verify the 'Log In' button is displayed
-      expect(find.widgetWithText(ElevatedButton, 'Log In'), findsOneWidget);
-
-      // Verify the 'Skip for now' text button is displayed
-      expect(find.widgetWithText(TextButton, 'Skip for now'), findsOneWidget);
-    });
-
-    testWidgets('FirstScreen Navigation Test', (WidgetTester tester) async {
-      final mockObserver = MockNavigatorObserver();
-      await tester.pumpWidget(MaterialApp(
-        home: FirstScreen(),
-        navigatorObservers: [mockObserver],
-      ));
-
-      // Tap the 'Sign Up' button and trigger a frame
-      await tester.tap(find.widgetWithText(ElevatedButton, 'Sign Up'));
-      await tester.pumpAndSettle();
-
-      // Verify that a push event happened on the navigation stack
-      verify(mockObserver.didPush(any, any));
-
-      // Tap the 'Log In' button and trigger a frame
-      await tester.tap(find.widgetWithText(ElevatedButton, 'Log In'));
-      await tester.pumpAndSettle();
-
-      // Verify that a push event happened on the navigation stack
-      verify(mockObserver.didPush(any, any));
-    });
   });
+
+
+  // Testing first_screen.dart
+  testWidgets('FirstScreen UI Test', (WidgetTester tester) async {
+    // Create the widget by telling the tester to build it
+    await tester.pumpWidget(MaterialApp(home: FirstScreen()));
+
+    // Verify the title is displayed
+    expect(find.text('Kaleidoscope Collaborative'), findsOneWidget);
+
+    // Verify the logo is displayed
+    expect(find.byType(Image), findsOneWidget);
+
+    // Verify the main message is displayed
+    expect(find.text('Discover Disability Inclusive\nServices Around You!'), findsOneWidget);
+
+    // Verify the 'Sign Up' button is displayed
+    expect(find.widgetWithText(ElevatedButton, 'Sign Up'), findsOneWidget);
+
+    // Verify the 'Log In' button is displayed
+    expect(find.widgetWithText(ElevatedButton, 'Log In'), findsOneWidget);
+
+    // Verify the 'Skip for now' text button is displayed
+    expect(find.widgetWithText(TextButton, 'Skip for now'), findsOneWidget);
+  });
+
+
+  // Testing login_screen UI
 }
 
