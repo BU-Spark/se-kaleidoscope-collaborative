724966bbc4be824fda05fd051907b32267853703
Timmy Lu <melontimmy101@gmail.com>
2024-04-19 19:46:29 -0400

    Minor whitespace changes, future support for org photo, added routing data from search page to review page data

--- kaleidoscope_collaborative/lib/screens/AddRating/review_page_1_1_overallRatingPage.dart
+++ kaleidoscope_collaborative/lib/screens/AddRating/review_page_1_1_overallRatingPage.dart
@@ -10,8 +10,15 @@ class AddReviewPage extends StatefulWidget {
   final String UserId;
   final String OrganizationId;
   final String OrgImgLink;
-  const AddReviewPage({Key? key, required this.OrganizationName, required this.OrganizationType, 
-  required this.UserId, required this.OrganizationId, required this.OrgImgLink}) : super(key: key);
+
+  const AddReviewPage(
+      {Key? key,
+      required this.OrganizationName,
+      required this.OrganizationType,
+      required this.UserId,
+      required this.OrganizationId,
+      required this.OrgImgLink})
+      : super(key: key);
 
   @override
   _AddReviewPageState createState() => _AddReviewPageState();
@@ -19,6 +26,7 @@ class AddReviewPage extends StatefulWidget {
 
 class _AddReviewPageState extends State<AddReviewPage> {
   int overallRating = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,13 +47,14 @@ class _AddReviewPageState extends State<AddReviewPage> {
             Row(
               children: <Widget>[
                 // Small Image on the top left corner
-                Image.asset(
-                  '${widget.OrgImgLink}',
-                  fit: BoxFit.cover,
-                  width: 117.0, 
-                  height: 99.0, 
+                FadeInImage.assetNetwork(
+                  placeholder: 'images/dental1.jpg',
+                  image: widget.OrgImgLink,
+                  fit: BoxFit.fill,
+                  width: 117.0,
+                  height: 99.0,
                 ),
-                SizedBox(width: 16.0), 
+                SizedBox(width: 16.0),
                 Expanded(
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
@@ -72,76 +81,75 @@ class _AddReviewPageState extends State<AddReviewPage> {
                 ),
               ],
             ),
-            Column( 
+            Column(
               // crossAxisAlignment: CrossAxisAlignment.stretch,
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 SizedBox(height: 20),
-            Text(
-              'How would you rate this business?',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 48),
-            
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: 
-              List.generate(5, (index) {
-                
-                return IconButton(
-                  icon: Stack(
-                    alignment: Alignment.center,
-                    children: <Widget>[
-                      // Circle icon as the background
-                      Icon(
-
-                        index < overallRating ? Icons.circle : Icons.circle,
-                        color: index < overallRating ? Color(0xFF6750A4) : Colors.grey,
-                        size: 60, 
-                      ),
-                      // Star icon on top of the circle
-                      Align(
+                Text(
+                  'How would you rate this business?',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 48),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: List.generate(5, (index) {
+                    return IconButton(
+                      icon: Stack(
                         alignment: Alignment.center,
-                        child: Icon(
-                          Icons.star,
-                          color: Colors.white, 
-                          size: 58, 
-                        ),
+                        children: <Widget>[
+                          // Circle icon as the background
+                          Icon(
+                            index < overallRating ? Icons.circle : Icons.circle,
+                            color: index < overallRating
+                                ? Color(0xFF6750A4)
+                                : Colors.grey,
+                            size: 60,
+                          ),
+                          // Star icon on top of the circle
+                          Align(
+                            alignment: Alignment.center,
+                            child: Icon(
+                              Icons.star,
+                              color: Colors.white,
+                              size: 58,
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
+                      onPressed: () {
+                        setState(() {
+                          overallRating = index + 1;
+                        });
+                      },
+                    );
+                  }),
+                ),
+                SizedBox(height: 48),
+                ElevatedButton(
                   onPressed: () {
-                    setState(() {
-                      overallRating = index + 1;
-                    });
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ChooseRatingParametersPage(
+                            overallRating: overallRating,
+                            OrganizationName: widget.OrganizationName,
+                            OrganizationType: widget.OrganizationType,
+                            UserId: widget.UserId,
+                            OrganizationId: widget.OrganizationId,
+                            OrgImgLink: widget.OrgImgLink),
+                      ),
+                    );
                   },
-                );
-              }),
-            ),
-            SizedBox(height: 48),
-
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ChooseRatingParametersPage(overallRating: overallRating,
-                    OrganizationName: widget.OrganizationName, OrganizationType: widget.OrganizationType, 
-                    UserId: widget.UserId, OrganizationId: widget.OrganizationId,
-                    OrgImgLink: widget.OrgImgLink),
-                  ),
-                );
-              },
-              child: Text('Next'),
-              style: kSmallButtonStyle,
-            ),
-            SizedBox(height: 16),
-
-            Align(
+                  child: Text('Next'),
+                  style: kSmallButtonStyle,
+                ),
+                SizedBox(height: 16),
+                Align(
                   alignment: Alignment.center,
                   child: TextButton(
                     onPressed: () => Navigator.of(context).pop(),
@@ -154,10 +162,10 @@ class _AddReviewPageState extends State<AddReviewPage> {
                     ),
                   ),
                 ),
+              ],
+            ),
           ],
         ),
-      ],
-      ),
       ),
     );
   }
