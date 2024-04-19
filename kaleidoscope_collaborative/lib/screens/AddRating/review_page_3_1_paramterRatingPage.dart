d62ce2b4e53539dab94b09f5fccbc807a5eeeb77
Timmy Lu <melontimmy101@gmail.com>
2024-04-19 19:40:56 -0400

    Minor whitespace changes, future support for org photo, centered star UI

--- kaleidoscope_collaborative/lib/screens/AddRating/review_page_3_1_paramterRatingPage.dart
+++ kaleidoscope_collaborative/lib/screens/AddRating/review_page_3_1_paramterRatingPage.dart
@@ -12,8 +12,16 @@ class ParameterRatingPage extends StatefulWidget {
   final String UserId;
   final String OrganizationId;
   final String OrgImgLink;
-  const ParameterRatingPage({Key? key, required this.parameterName,required this.OrganizationName, required this.OrganizationType, 
-  required this.UserId, required this.OrganizationId , required this.OrgImgLink}) : super(key: key);
+
+  const ParameterRatingPage(
+      {Key? key,
+      required this.parameterName,
+      required this.OrganizationName,
+      required this.OrganizationType,
+      required this.UserId,
+      required this.OrganizationId,
+      required this.OrgImgLink})
+      : super(key: key);
 
   @override
   _ParameterRatingPageState createState() => _ParameterRatingPageState();
@@ -21,11 +29,13 @@ class ParameterRatingPage extends StatefulWidget {
 
 class _ParameterRatingPageState extends State<ParameterRatingPage> {
   int parameterRating = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Accommodation Rating Page', style: TextStyle(color: Colors.black)),
+        title: Text('Accommodation Rating Page',
+            style: TextStyle(color: Colors.black)),
         leading: IconButton(
           icon: Icon(Icons.arrow_back, color: Colors.black),
           onPressed: () => Navigator.of(context).pop(),
@@ -40,13 +50,14 @@ class _ParameterRatingPageState extends State<ParameterRatingPage> {
           children: <Widget>[
             Row(
               children: <Widget>[
-                Image.asset(
-                  '${widget.OrgImgLink}',
-                  fit: BoxFit.cover,
+                FadeInImage.assetNetwork(
+                  placeholder: 'images/dental1.jpg',
+                  image: widget.OrgImgLink,
+                  fit: BoxFit.fill,
                   width: 117.0,
                   height: 99.0,
                 ),
-                SizedBox(width: 16.0), 
+                SizedBox(width: 16.0),
                 // Organization Title and Type
                 Expanded(
                   child: Column(
@@ -74,84 +85,82 @@ class _ParameterRatingPageState extends State<ParameterRatingPage> {
                 ),
               ],
             ),
-            Column( 
+            Column(
               // crossAxisAlignment: CrossAxisAlignment.stretch,
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 SizedBox(height: 20),
-            Text(
-              'How would you rate ${widget.parameterName} at this business?',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 48),
-            
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: 
-              List.generate(5, (index) {
-                
-                return IconButton(
-                  icon: Stack(
-                    alignment: Alignment.center,
-                    children: <Widget>[
-                      // Circle icon as the background
-                      Icon(
-
-                        index < parameterRating ? Icons.circle : Icons.circle,
-                        color: index < parameterRating ? Color(0xFF6750A4) : Colors.grey,
-                        size: 60, 
-                      ),
-                      // Star icon on top of the circle
-                      Align(
-                        alignment: Alignment.center,
-                        child: Icon(
-                          Icons.star,
-                          color: Colors.white, // The color for the circle
-                          size: 58, // The size of the circle
-                        ),
-                      ),
-                    ],
+                Text(
+                  'How would you rate ${widget.parameterName} at this business?',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
                   ),
-                  onPressed: () {
-                    setState(() {
-                      parameterRating = index + 1;
-                    });
-                  },
-                );
-              }),
-            ),
-            SizedBox(height: 48),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                ElevatedButton(
-                  onPressed: () {
-                    // Implement skip logic
-                     Navigator.pop(context, null); // Return null to indicate skipping
-                  },
-                  child: Text('Skip'),
-                  style: kSmallButtonStyle,
                 ),
-                ElevatedButton(
-                  onPressed: () {
-                    // Implement next logic
-                    Navigator.pop(context, parameterRating); // Return the rating
-                  },
-                  child: Text('Next'),
-                  style: kSmallButtonStyle,
+                SizedBox(height: 48),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: List.generate(5, (index) {
+                    return IconButton(
+                      icon: Stack(
+                        alignment: Alignment.center,
+                        children: <Widget>[
+                          // Circle icon as the background
+                          Icon(
+                            index < parameterRating
+                                ? Icons.circle
+                                : Icons.circle,
+                            color: index < parameterRating
+                                ? Color(0xFF6750A4)
+                                : Colors.grey,
+                            size: 60,
+                          ),
+                          // Star icon on top of the circle
+                          Align(
+                            alignment: Alignment.center,
+                            child: Icon(
+                              Icons.star,
+                              color: Colors.white, // The color for the circle
+                              size: 58, // The size of the circle
+                            ),
+                          ),
+                        ],
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          parameterRating = index + 1;
+                        });
+                      },
+                    );
+                  }),
                 ),
-
-              ],
-
-            ),
-            SizedBox(height: 16),
-
-            Align(
+                SizedBox(height: 48),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    ElevatedButton(
+                      onPressed: () {
+                        // Implement skip logic
+                        Navigator.pop(
+                            context, null); // Return null to indicate skipping
+                      },
+                      child: Text('Skip'),
+                      style: kSmallButtonStyle,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Implement next logic
+                        Navigator.pop(
+                            context, parameterRating); // Return the rating
+                      },
+                      child: Text('Next'),
+                      style: kSmallButtonStyle,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 16),
+                Align(
                   alignment: Alignment.center,
                   child: TextButton(
                     onPressed: () => Navigator.of(context).pop(),
@@ -164,10 +173,10 @@ class _ParameterRatingPageState extends State<ParameterRatingPage> {
                     ),
                   ),
                 ),
+              ],
+            ),
           ],
         ),
-      ],
-      ),
       ),
     );
   }
