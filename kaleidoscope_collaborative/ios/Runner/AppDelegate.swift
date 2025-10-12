ec7364871b30eee232add10f595f23a522382338
suranabhavya <bhavyasurana2000@gmail.com>
2025-10-12 13:53:56 -0400

    updating versions and making it work

--- kaleidoscope_collaborative/ios/Runner/AppDelegate.swift
+++ kaleidoscope_collaborative/ios/Runner/AppDelegate.swift
@@ -5,7 +5,7 @@ import GoogleSignIn
 
 // import GoogleMaps
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
